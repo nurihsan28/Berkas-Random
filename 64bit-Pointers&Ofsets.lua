51e1b27f843fd7f9f8730be18188cd8b8a9f5743
nurihsan28 <28ihsan3@gmail.com>
2025-01-09 14:13:35 +0700

    Create 64bit-Pointers&Ofsets.lua
    64bit-Pointers&Ofsets.lua

---
+++ 64bit-Pointers&Ofsets.lua
@@ -0,0 +1,7 @@
+UP_A = {"h 00 0C 40 F9 C0 03 5F D6 C0 03 5F D6 01 99 00 D0 21 00 0B 91 01 00 00 F9 95 C4 07 14 F3 7B BF A9 01 99 00 D0 F3 03 00 AA 21 00 0B 91 01 00 00 F9 8F C4 07 94 E0 03 13 AA F3 7B C1 A8 A3 41 43 14"}
+UP_C = {"h 00 A0 05 D1 F7 FD FF 17 F3 7B BF A9 F3 03 00 AA F4 FD FF 97 E0 03 13 AA F3 7B C1 A8 AB B7 25 14 00 A0 05 D1 F9 FF FF 17 F3 53 BB A9 F3 F3 01 B2 E4 F3 00 B2 F5 5B 01 A9 F7 63 02 A9 F9 7B 03 A9"}
+timeReseterOfsets = {"0x9c", "0x1b8", "0x1c0", "0x1c8"}
+freezeOfsets = {"-0x104", "-0x108", "-0x10c", "-0x110", "0x168", "0x16c", "0x170", "0x174", "0x178", "0x17c", "0x180", "0x184", "0x188", "0x18c", "0x190", "0x194", "0x1a8", "0x1ac", "0x1b0", "0x1b4", "0x1b8", "0x1bc", "0x1c0", "0x1c4"}
+wrcOfsets = {"-0x8", "0x288", "0x290", "0x298", "0x2d8", "0x2e0", "0x2e8", "0x308", "0x310", "0x318", "0x338", "0x340", "0x348", "0x3c", "0x30c", "0x314", "0x31c"}
+UGV = {"1.60.1.133743"}
+onlineAnnouncement = {"Announcement: Needed should buy me a coffee atleast minimum 10 people. Otherwise I won't update this project. You already know my Patreon. I need money man. If you really want to, update it yourself."}
